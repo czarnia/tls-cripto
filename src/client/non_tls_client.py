b76848a7474073df841ef03d63726b3760fc0588
czarnia <czarniana@gmail.com>
2020-06-06 19:08:01 -0300

    Basic non tls client and server added

--- src/client/non_tls_client.py
+++ src/client/non_tls_client.py
@@ -1,1 +1,22 @@
+import sys
+sys.path.append('../')
 
+from common.string_socket import StringSocket
+
+def main():
+	skt = StringSocket()
+
+	skt.connect("server", 8080)
+	print("Soy el cliente y me conecte al servidor")
+
+	msg = skt.receive_with_size()
+	print(f"Soy el cliente y recibi: {msg}")
+
+	print("Soy el cliente y voy a enviar")
+
+	skt.send_with_size("Hola servidor!")
+
+	skt.close()
+
+if __name__ == '__main__':
+	main()
