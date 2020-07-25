fac9fd524e54e9449f0ee140b0c2a6f6a093064a
Ana Czarnitzki <ana.czarnitzki@wildlifestudios.com>
2020-07-24 21:35:02 -0300

    No more global variables

--- src/client/poodle_client.py
+++ src/client/poodle_client.py
@@ -14,28 +14,22 @@ from common.string_socket import StringSocket
 SECRET = "auth:CRIPTO2020"
 LENGTH_BLOCK = 16
 
-IV = Random.new().read( AES.block_size )
-KEY = Random.new().read( AES.block_size )
-
-# generate random key and iv
+# generate random value for keys and ivs
 def randkey():
-    global IV 
-    IV = Random.new().read( AES.block_size )
-    global KEY 
-    KEY = Random.new().read( AES.block_size )
+    return Random.new().read(AES.block_size)
 
 # padding for the CBC cipher block
 def pad(s):
     return (16 - len(s) % 16) * chr((16 - len(s) - 1) % 16)
 
 # cipher a message
-def encrypt( msg):
+def encrypt(msg, key, iv):
     data = msg.encode()
-    hash = hmac.new(KEY, data, hashlib.sha256).digest()
+    hash = hmac.new(key, data, hashlib.sha256).digest()
     padding = pad(data + hash)
     raw = data + hash + padding.encode()
-    cipher = AES.new(KEY, AES.MODE_CBC, IV)
-    return cipher.encrypt( raw )
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return cipher.encrypt(raw)
 
 def main():
 	skt = StringSocket()
@@ -44,12 +38,14 @@ def main():
 	print("Soy el cliente y me conecté.")
 
 	# fill the last block with full padding 0f
-	t = binascii.hexlify(encrypt(SECRET))
+	key = randkey()
+	iv = randkey()
+	t = binascii.hexlify(encrypt(SECRET, key, iv))
 	original_length = len(t)
 
 	t = 1
 	while True:
-		length = len(binascii.hexlify(encrypt("a"*t + SECRET)))
+		length = len(binascii.hexlify(encrypt("a"*t + SECRET, key, iv)))
 		if(length > original_length):
 			break
 		t += 1
@@ -58,14 +54,14 @@ def main():
 	for block in range(original_length//32-2,0,-1):
 		for char in range(LENGTH_BLOCK):
 			while True:
-				randkey()
+				key = randkey()
+				iv = randkey()
 
-				# Envío el bloque encriptado
-				skt.send_with_size(binascii.hexlify(encrypt("$"*16 + "#"*t + SECRET + "%"*(block*LENGTH_BLOCK - char))).hex())
-				# Envío el initializator para luego simular si el server "acepta" el paquete o no.
-				skt.send_with_size(IV.hex())
-				# Envío la clave para luego simular si el server "acepta" el paquete o no.
-				skt.send_with_size(KEY.hex())
+				# Sending the encripted block
+				skt.send_with_size(binascii.hexlify(encrypt("$"*16 + "#"*t + SECRET + "%"*(block*LENGTH_BLOCK - char), key, iv)).hex())
+				# Sending the iv and key to simulate if the server accepts the packets or not
+				skt.send_with_size(iv.hex())
+				skt.send_with_size(key.hex())
 
 				msg = skt.receive_with_size()
 				if (msg == "Adivinado"):
